2500c6f6b713462aade216e857195e9ed4dbac6b
Maximo0104 <maximelchor@gmail.com>
2020-05-24 19:36:38 -0300

    pagina web

--- index.py
+++ index.py
@@ -11,6 +11,35 @@ def about ():
 @app.route('/comidas')
 def comidas():
     return render_template('comidas.html')
+@app.route ('/contacto')
+def contacto():
+    return render_template ('contacto.html')
+@app.route ('/tips')
+def tips():
+    return render_template('tips.html')
+@app.route('/pecho')
+def pecho():
+    return render_template('pecho.html')
+@app.route('/brazos')
+def brazos():
+    return render_template('brazos.html')
+@app.route('/abs')
+def abs():
+    return render_template('abs.html')
+@app.route('/hombros')
+def hombros():
+    return render_template('hombros.html')
+@app.route('/espalda')
+def espalda():
+    return render_template('espalda.html')
+@app.route('/piernas')
+def piernas():
+    return render_template('piernas.html')
+
+
+
+
+
 
 
 if __name__ == '__main__':
